9d9b029aba5c5ed63c229980b64eb1bd580ec948
nstyPlyaskin <4deptmusic@gmail.com>
2023-11-21 22:54:07 +0700

    Support bugfix: removing cancel button after using

--- src/func/supportChatSender.py
+++ src/func/supportChatSender.py
@@ -2,6 +2,13 @@ from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 CHAT_ID_SUPPORT = '-1002082264985'
 
+# Список команд для управления
+MANAGEMENT_COMMANDS = ['Информация об артисте', 'Загрузить новый релиз']
+
+def is_management_command(text):
+    # Проверяем, является ли текст командой для управления
+    return text.lower() in [command.lower() for command in MANAGEMENT_COMMANDS]
+
 def setup_support_handler(bot):
     # Создаем словарь для отслеживания статуса обработки вопроса для каждого пользователя
     awaiting_question = {}
@@ -24,28 +31,34 @@ def setup_support_handler(bot):
 
         # Запрашиваем вопрос у пользователя
         answer_text = "Введите вопрос для команды поддержки или нажмите 'Отмена' для отмены:"
-        bot.send_message(chat_id=message.chat.id, text=answer_text, reply_markup=keyboard)
+        msg = bot.send_message(chat_id=message.chat.id, text=answer_text, reply_markup=keyboard)
+
+        # Сохраняем идентификатор сообщения с клавиатурой "Отмена" для последующего редактирования
+        awaiting_question[message.chat.id] = msg.message_id
 
     @bot.message_handler(func=lambda m: awaiting_question.get(m.from_user.id, False))
     def handle_support_question(message):
         user_question = message.text
 
+        # Проверяем, не является ли вопрос командой для управления
+        if is_management_command(user_question):
+            bot.send_message(message.chat.id, "Извините, но эта команда не может быть отправлена в поддержку. Для отмены используйте кнопку 'Отмена'")
+            return
+
         # Отправляем информацию в чат поддержки
-        support_message = f"Пользователь {message.chat.first_name} {message.chat.last_name} с ID {message.chat.id} отправил вопрос: {user_question}"
+        support_message = f"Пользователь {message.chat.first_name} {message.chat.last_name} (ID {message.chat.id}) отправил вопрос: {user_question}"
         bot.send_message(CHAT_ID_SUPPORT, support_message)
 
-        # Отвечаем пользователю подтверждением
-        confirmation_text = "Ваш вопрос отправлен в поддержку. Мы просматриваем каждый вопрос и ответим вам в ближайшее время."
-        bot.send_message(message.chat.id, confirmation_text)
-
-        # Сбрасываем статус ожидания для этого пользователя
-        awaiting_question.pop(message.chat.id, None)
+        # Удаляем клавиатуру "Отмена" после успешной отправки запроса
+        bot.edit_message_text(chat_id=message.chat.id, message_id=awaiting_question.pop(message.chat.id),
+                              text="Ваш вопрос отправлен в поддержку. Мы просматриваем каждый вопрос и ответим вам в ближайшее время.")
 
     @bot.callback_query_handler(func=lambda call: call.data == "cancel")
     def cancel_support(call):
         # Если пользователь нажал "Отмена" во время запроса в поддержку
         awaiting_question.pop(call.message.chat.id, None)
-        bot.send_message(call.message.chat.id, "Отправка в поддержку отменена.")
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text="Отправка в поддержку отменена.")
 
     # Завершаем обработку сообщения
     return
