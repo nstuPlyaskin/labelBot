0676f905fe22b971d2adeb84c1ccc8c41e9b9753
nstuPlyaskin <4deptmusic@gmail.com>
2024-03-21 04:20:44 +0700

    Added: answer to questions
    Added: /a to answer

--- src/func/dbAction.py
+++ src/func/dbAction.py
@@ -32,16 +32,56 @@ class DB:
         return self.conn.commit()
     
     def showQuestions(self, bot, message):
-        self.cursor.execute("SELECT id, userName, userQuestion, userMedia, date FROM supportTable WHERE isActual = 1")
-        rows = self.cursor.fetchall()
-        for row in rows:
+        # Получаем количество актуальных вопросов в базе данных
+        self.cursor.execute("SELECT COUNT(*) FROM supportTable WHERE isActual = 1")
+        total_questions = self.cursor.fetchone()[0]
+
+        # Проверяем, есть ли вопросы в базе данных
+        if total_questions > 0:
+            # Выполняем запрос для получения последнего вопроса
+            self.cursor.execute("SELECT id, userName, userQuestion, userMedia, date FROM supportTable WHERE isActual = 1 ORDER BY id DESC LIMIT 1")
+            row = self.cursor.fetchone()
             id, username, question, usermedia, date = row
+
             # Отправляем сообщение с подписью к изображению, если userMedia не пустое
             if usermedia:
-                bot.send_photo(chat_id=message.chat.id, photo=usermedia, caption=f"Вопрос №{id} от пользователя: {username}\n\nQuestion: {question}\n\nВопрос был задан {date}")
+                bot.send_photo(chat_id=message.chat.id, photo=usermedia, caption=f"Вопрос №{id} от пользователя: {username}\n\nQuestion: {question}\n\nВопрос был задан {date}\n\nОсталось вопросов в очереди: {total_questions - 1}\nВопрос был задан {date}\n\nДля ответа используйте /answer (номер вопроса) и текст ответа.\nПример: /answer 69 Здравствуйте, я ответил на ваш вопрос.")
             # Иначе отправляем текстовое сообщение
             else:
-                bot.send_message(chat_id=message.chat.id, text=f"Вопрос №{id} от пользователя: {username}\n\nQuestion: {question}\n\nВопрос был задан {date}")
+                bot.send_message(chat_id=message.chat.id, text=f"Вопрос №{id} от пользователя: {username}\n\nQuestion: {question}\n\nОсталось вопросов в очереди: {total_questions - 1}\nВопрос был задан {date}\n\nДля ответа используйте /answer (номер вопроса) и текст ответа.\nПример: /answer 69 Здравствуйте, я ответил на ваш вопрос.")
+        else:
+            bot.send_message(chat_id=message.chat.id, text="На данный момент вопросов нет.")
+
+
+    def getAnswer(self, bot, message):
+        # Разбиваем сообщение на аргументы (номер вопроса и текст ответа)
+        args = message.text.split(maxsplit=2)
+        if len(args) < 3:
+            bot.reply_to(message, "Вы должны указать номер вопроса и текст ответа. Пример: /answer 69 Здравствуйте, я ответил на ваш вопрос.")
+            return
+
+        # Получаем номер вопроса и текст ответа из аргументов
+        question_id = args[1]
+        answer_text = args[2]
+
+        # Проверяем, существует ли вопрос с указанным номером
+        self.cursor.execute("SELECT * FROM supportTable WHERE id = ? AND isActual = 1", (question_id,))
+        question = self.cursor.fetchone()
+        if question:
+            # Обновляем запись в базе данных с ответом на вопрос и помечаем вопрос как отвеченный
+            self.cursor.execute("UPDATE supportTable SET userAnswer = ?, isActual = 0 WHERE id = ?", (answer_text, question_id))
+            self.conn.commit()
+
+            # Отправляем ответ пользователю, включая вопрос
+            user_id = question[1]  # Предполагается, что user_id хранится во втором столбце таблицы
+            question_text = question[3]  # Предполагается, что текст вопроса находится в четвертом столбце таблицы
+            full_answer = f"Ответ на ваш вопрос '{question_text}':\n{answer_text}\n\nС уважением, команда ETALON MUSIC"
+            bot.send_message(chat_id=user_id, text=full_answer)
+            
+            # Уведомляем о том, что ответ отправлен
+            bot.reply_to(message, "Ответ на вопрос успешно отправлен.")
+        else:
+            bot.reply_to(message, "Вопрос с указанным номером не найден.")
 
 
     def close (self):
