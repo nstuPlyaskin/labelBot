69203174e42cddc91ac27da2426f36d1a0849a29
nstuPlyaskin <4deptmusic@gmail.com>
2024-03-21 03:35:09 +0700

    Added: protection admin cmds
    Added: whitelist with uid able to use /q

--- src/func/admQuestions.py
+++ src/func/admQuestions.py
@@ -1,10 +1,21 @@
 from telebot import TeleBot
 from func.dbAction import DB
-import os
+import os, json
 
 db_path = os.path.join(os.path.dirname(__file__), '..', 'db', 'support')
+whitelist_path = os.path.join(os.path.dirname(__file__), '..', 'db', 'whitelist.json')
+
+
+def is_user_allowed(user_id):
+    with open(whitelist_path, "r") as f:
+        whitelist = json.load(f)
+    allowed_users = whitelist.get("allowed_users", [])
+    return user_id in allowed_users
 
 def setup_admQuestions_handler(bot: TeleBot, message):
-    db = DB(db_path)  # Создаем экземпляр класса DB с путем к базе данных
-    db.showQuestions(bot, message)  # Вызываем метод showQuestions с объектом бота
-    db.close()  # Закрываем соединение с базой данных после использования
+    if is_user_allowed(message.from_user.id):
+        db = DB(db_path)  # Создаем экземпляр класса DB с путем к базе данных
+        db.showQuestions(bot, message)  # Вызываем метод showQuestions с объектом бота
+        db.close()  # Закрываем соединение с базой данных после использования
+    else:
+        bot.reply_to(message, "Извините, у вас нет доступа к этой команде.")
