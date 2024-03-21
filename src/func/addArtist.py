59c94a886deea05cc05b2f489f5634fb02317fc2
nstuPlyaskin <4deptmusic@gmail.com>
2024-03-22 03:14:35 +0700

    Added: cancel button while creating new artist
    Added: fuction for cancel creating new artist

--- src/func/addArtist.py
+++ src/func/addArtist.py
@@ -27,15 +27,14 @@ def send_next_question(bot: TeleBot, message: Message):
     global current_question_index, user_data
     
     if current_question_index < len(questions):
-        # Проверяем, есть ли uid в объекте сообщения
         if message.from_user.id:
-            # Добавляем uid в user_data
             user_data['uid'] = message.from_user.id
         
-        bot.send_message(message.chat.id, questions[current_question_index])
+        keyboard = get_cancel_keyboard() if current_question_index > 0 else None
+        
+        bot.send_message(message.chat.id, questions[current_question_index], reply_markup=keyboard)
         bot.register_next_step_handler(message, save_user_answer, bot=bot)
     else:
-        # Все вопросы заданы, отправляем данные в функцию addArtist
         db = DB(db_path)
         success = db.addArtist(user_data)
         db.close()
@@ -45,33 +44,42 @@ def send_next_question(bot: TeleBot, message: Message):
         else:
             bot.send_message(message.chat.id, "Произошла ошибка при добавлении артиста.")
 
-
 # Функция для сохранения ответа пользователя и перехода к следующему вопросу
-from func.dbAction import DB
-
 def save_user_answer(message: Message, bot: TeleBot):
     global current_question_index, user_data
+    
+    # Проверяем, написал ли пользователь "Отмена"
+    if message.text.strip().lower() == "отмена":
+        bot.send_message(message.chat.id, "Процедура создания артиста отменена.", reply_markup=types.ReplyKeyboardRemove())
+        return
     
     current_key = keys[current_question_index]
     user_answer = message.text.strip()
     
-    # Проверяем, существует ли артист с указанным никнеймом
     if current_key == "artistNickName":
         db = DB(db_path)
         if db.check_artist_exists(user_answer):
             bot.send_message(message.chat.id, "Такой артист уже существует. Пожалуйста, введите другой никнейм:")
-            # Регистрируем обработчик для следующего ответа пользователя на тот же вопрос
             bot.register_next_step_handler(message, save_user_answer, bot=bot)
-            return  # Выходим из функции, чтобы дождаться нового ответа пользователя
+            return
     
     user_data[current_key] = user_answer
     current_question_index += 1
     send_next_question(bot, message)
 
 
-# Основная функция для начала опроса
+# Функция для начала процедуры создания нового артиста
 def setup_addArtist_handler(bot: TeleBot, message: Message):
     global current_question_index, user_data
     current_question_index = 0
     user_data.clear()
+    keyboard = get_cancel_keyboard()
+    bot.send_message(message.chat.id, "Начата процедура создания нового артиста, для отмены напишите 'Отмена'.", reply_markup=keyboard)
     send_next_question(bot, message)
+
+# Функция для получения клавиатуры с кнопкой "Отмена"
+def get_cancel_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_cancel = types.KeyboardButton(text="Отмена")
+    keyboard.add(button_cancel)
+    return keyboard
