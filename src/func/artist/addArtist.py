483632c85c8cf50f093608e84d6d7491dc551c43
nstuPlyaskin <4deptmusic@gmail.com>
2024-03-23 01:58:23 +0700

    Bugfix: several users not cant crash bot
    Fixed: addArtist script, bug were in var that count current question index. Now every user have personal var to contain this index

--- src/func/artist/addArtist.py
+++ src/func/artist/addArtist.py
@@ -24,23 +24,20 @@ confrimQuestions = [
 
 keys = ["artistNickName", "artistRealName", "artistSpotify", "artistContacts"]
 
-# Переменная для хранения текстового описания информации, введенной пользователем
-questions_summary = []
-
-# Счетчик для отслеживания текущего вопроса
-current_question_index = 0
-
 # Словарь для хранения данных пользователя
-user_data = {}
+user_states = {}
 
 # Функция для отправки следующего вопроса и регистрации обработчика ответа
 def send_next_question(bot: TeleBot, message: Message):
-    global current_question_index, user_data
+    user_id = message.from_user.id
+    if user_id not in user_states:
+        user_states[user_id] = {'current_question_index': 0, 'user_data': {}, 'questions_summary': []}
+        
+    current_question_index = user_states[user_id]['current_question_index']
+    user_data = user_states[user_id]['user_data']
+    questions_summary = user_states[user_id]['questions_summary']
     
     if current_question_index < len(questions):
-        if message.from_user.id:
-            user_data['uid'] = message.from_user.id
-        
         keyboard = get_cancel_keyboard() if current_question_index > 0 else None
         
         bot.send_message(message.chat.id, questions[current_question_index], reply_markup=keyboard)
@@ -50,11 +47,15 @@ def send_next_question(bot: TeleBot, message: Message):
 
 # Функция для сохранения ответа пользователя и перехода к следующему вопросу
 def save_user_answer(message: Message, bot: TeleBot):
-    global current_question_index, user_data, questions_summary
+    user_id = message.from_user.id
+    current_question_index = user_states[user_id]['current_question_index']
+    user_data = user_states[user_id]['user_data']
+    questions_summary = user_states[user_id]['questions_summary']
     
     # Проверяем, написал ли пользователь "Отмена"
     if message.text.strip().lower() == "отмена":
         bot.send_message(message.chat.id, "Процедура создания артиста отменена.", reply_markup=get_main_keyboard())
+        del user_states[user_id]
         return
     
     current_key = keys[current_question_index]
@@ -70,11 +71,16 @@ def save_user_answer(message: Message, bot: TeleBot):
     user_data[current_key] = user_answer
     questions_summary.append(f"{questions[current_question_index]} {user_answer}")
     current_question_index += 1
+    user_states[user_id]['current_question_index'] = current_question_index
+    user_states[user_id]['user_data'] = user_data
+    user_states[user_id]['questions_summary'] = questions_summary
     send_next_question(bot, message)
 
 # Функция для отправки сообщения с подтверждением введенной информации
 def send_confirmation_message(bot: TeleBot, message: Message):
-    global user_data, questions_summary, confrimQuestions
+    user_id = message.from_user.id
+    user_data = user_states[user_id]['user_data']
+    questions_summary = user_states[user_id]['questions_summary']
 
     confirmation_text = ""
     for question, summary in zip(confrimQuestions, questions_summary):
@@ -87,7 +93,9 @@ def send_confirmation_message(bot: TeleBot, message: Message):
 
 # Функция для обработки ответа пользователя на подтверждение информации
 def handle_confirmation_response(message: Message, bot: TeleBot):
+    user_id = message.from_user.id
     if message.text.lower() == "да":
+        user_data = user_states[user_id]['user_data']
         db = DB(db_path)
         success = db.addArtist(user_data)
         db.close()
@@ -96,12 +104,14 @@ def handle_confirmation_response(message: Message, bot: TeleBot):
             bot.send_message(message.chat.id, "Артист успешно добавлен.", reply_markup=get_existing_artist_keyboard())
         else:
             bot.send_message(message.chat.id, "Произошла ошибка при добавлении артиста.")
+        del user_states[user_id]
     elif message.text.lower() == "нет":
         # Пользователь хочет изменить информацию, начинаем процесс сначала
+        del user_states[user_id]
         setup_addArtist_handler(bot, message)
     else:
         bot.send_message(message.chat.id, "Пожалуйста, введите 'Да' или 'Нет'.")
-        
+
 # Функция для создания клавиатуры с кнопками "Да" и "Нет" для подтверждения информации
 def get_confirmation_keyboard():
     keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
@@ -112,16 +122,9 @@ def get_confirmation_keyboard():
 
 # Функция для начала процедуры создания нового артиста
 def setup_addArtist_handler(bot: TeleBot, message: Message):
-    global current_question_index, user_data, questions_summary
-    clear_user_data()
+    user_id = message.from_user.id
+    user_states[user_id] = {'current_question_index': 0, 'user_data': {'uid': user_id}, 'questions_summary': []}
     keyboard = get_cancel_keyboard()
     bot.send_message(message.chat.id, "Начата процедура создания нового артиста, для отмены напишите 'Отмена'.", reply_markup=keyboard)
     send_next_question(bot, message)
-
-
-def clear_user_data():
-    global current_question_index, user_data, questions_summary
-    current_question_index = 0
-    user_data.clear()
-    questions_summary.clear()
 
